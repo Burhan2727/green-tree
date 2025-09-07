2306bb5f7c80c7ac709ba5763ccbcc541de1f066
Burhan <burhan.umt@gmail.com>
2025-09-07 21:21:48 +0300

    customize categories

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const displayCategories = (categories) => {
   for (const categori of categories) {
     const btnDiv = document.createElement("div")
     btnDiv.innerHTML = `
-    <a id="btn-active-${categori.id}" class=" hover:bg-[#15803D] hover:text-white md:w-full text-[#1F2937] cursor-pointer block rounded-sm p-3">${categori.category_name}
+    <a id="btn-active-${categori.id}" class=" hover:bg-[#15803D] hover:text-white md:w-full text-[#1F2937] cursor-pointer inline-block md:block rounded-sm p-3">${categori.category_name}
     </a>
     `;
     categoriesContainer.appendChild(btnDiv)
