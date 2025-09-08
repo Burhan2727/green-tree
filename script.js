c27972062d792376b5a541298d3a9c685931fd22
Burhan <burhan.umt@gmail.com>
2025-09-08 21:36:54 +0300

    added categories, all plants, modal functionalitys

--- script.js
+++ script.js
@@ -7,16 +7,141 @@ const loadCategories = () => {
 const displayCategories = (categories) => {
   const categoriesContainer = document.getElementById
   ("categories-container");
-  console.log(categories)
+  // console.log(categories)
   for (const categori of categories) {
     const btnDiv = document.createElement("div")
     btnDiv.innerHTML = `
-    <a id="btn-active-${categori.id}" class=" hover:bg-[#15803D] hover:text-white md:w-full text-[#1F2937] cursor-pointer inline-block md:block rounded-sm p-3">${categori.category_name}
+    <a onclick="categoriLoadData(${categori.id})" id="btn-active-${categori.id}" class=" hover:bg-[#15803D] hover:text-white md:w-full text-[#1F2937] cursor-pointer inline-block md:block rounded-sm p-3">${categori.category_name}
     </a>
     `;
     categoriesContainer.appendChild(btnDiv)
   }
 
 };
+const categoriLoadData = (id)=>{
+  const url = `https://openapi.programming-hero.com/api/category/${id}`
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayCategoriData(data.plants))
+}
+const displayCategoriData = (plants)=>{
+  const plantsCard = document.getElementById("plants-card");
+  plantsCard.innerHTML = ""
+  for(const plant of plants){
+    const plantDiv = document.createElement("div");
+    plantDiv.innerHTML = `
+    <div class="bg-white p-3 rounded-lg space-y-3 h-full flex flex-col">
+                <img class ="h-48 w-full object-cover rounded-lg" src="${plant.image}" alt="" />
+                <h2 onclick="treeLoadDetails(${plant.id})" class="font-semibold text-lg cursor-pointer">
+                ${plant.name}
+                </h2>
+                <p class="text-sm flex-grow">
+                  ${plant.description}
+                </p>
+                <div class="flex justify-between">
+                  <span
+                    class="bg-[#DCFCE7] p-1 rounded-2xl text-[#15803D] text-sm font-medium font-geist"
+                    >${plant.category}</span
+                  >
+                  <p class="font-semibold text-lg">৳<span>${plant.price}</span></p>
+                </div>
+                <button class="btn bg-[#15803D] w-full rounded-2xl text-white">
+                  Add to Cart
+                </button>
+              </div>
+    
+    `
+    plantsCard.appendChild(plantDiv)
+  }
+}
 loadCategories();
 // all categories functionality end
+
+
+
+// categories plants funtionality start
+// categories plants funtionality end
+
+// all plants funtionality start
+const loadPlants = ()=>{
+  fetch("https://openapi.programming-hero.com/api/plants")
+  .then(res => res.json())
+  .then(data => displayPlants(data.plants))
+}
+// {
+//     "id": 1,
+//     "image": "https://i.ibb.co.com/cSQdg7tf/mango-min.jpg",
+//     "name": "Mango Tree",
+//     "description": "A fast-growing tropical tree that produces delicious, juicy mangoes during summer. Its dense green canopy offers shade, while its sweet fruits are rich in vitamins and minerals.",
+//     "category": "Fruit Tree",
+//     "price": 500
+// }
+const displayPlants = (plants)=>{
+  // console.log(plants)
+  const plantsCard = document.getElementById("plants-card");
+  for(const plant of plants){
+    const plantDiv = document.createElement("div");
+    plantDiv.innerHTML = `
+    <div class="bg-white p-3 rounded-lg space-y-3 h-full flex flex-col">
+                <img class ="h-48 w-full object-cover rounded-lg" src="${plant.image}" alt="" />
+                <h2 onclick="treeLoadDetails(${plant.id})" class="font-semibold text-lg cursor-pointer">
+                ${plant.name}
+                </h2>
+                <p class="text-sm flex-grow">
+                  ${plant.description}
+                </p>
+                <div class="flex justify-between">
+                  <span
+                    class="bg-[#DCFCE7] p-1 rounded-2xl text-[#15803D] text-sm font-medium font-geist"
+                    >${plant.category}</span
+                  >
+                  <p class="font-semibold text-lg">৳<span>${plant.price}</span></p>
+                </div>
+                <button class="btn bg-[#15803D] w-full rounded-2xl text-white">
+                  Add to Cart
+                </button>
+              </div>
+    
+    `
+    plantsCard.appendChild(plantDiv)
+  }
+}
+loadPlants()
+// all plants funtionality end
+// {
+//     "id": 30,
+//     "image": "https://i.ibb.co.com/0jLycYdv/Water-Hyacinth-min.jpg",
+//     "name": "Water Hyacinth",
+//     "description": "A floating plant with violet flowers that provide shade to aquatic creatures. Known for rapid growth in ponds.",
+//     "category": "Aquatic Plant",
+//     "price": 250
+// }
+// plants modal funtionality start
+const treeLoadDetails = (id)=>{
+  const url = `https://openapi.programming-hero.com/api/plant/${id}`
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayTreeDetails(data.plants
+))
+}
+const displayTreeDetails = (tree)=>{
+  const detailsContainer = document.getElementById("details-container")
+  detailsContainer.innerHTML = `
+  <div class="bg-white p-3 rounded-lg space-y-3 h-full flex flex-col">
+            <h2 class="font-semibold text-lg">${tree.name}</h2>
+            <img
+              class="h-48 w-full object-cover rounded-lg"
+              src="${tree.image}"
+              alt=""
+            />
+            <p><span class="font-semibold">Category:</span> ${tree.category}</p>
+            <p><span class="font-semibold">Price:</span> ${tree.price}</p>
+            <p class="text-sm flex-grow">
+              <span class="font-semibold">Description:</span> ${tree.description}
+            </p>
+          </div>
+  `
+  document.getElementById("my_modal_5").showModal()
+}
+
+// plants modal funtionality end
